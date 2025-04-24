98b56604d3cd34307ba4ecdd4bbe66a280635b42
agunggst <rqz.agastya@gmail.com>
2025-04-24 20:21:13 +0700

    .

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # --- CORS config ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://abang-shopeebot.web.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
